fab8f04464361d379b4ab2aeb3c9b7e3614cdd2a
Maximilian <rittermax@gmail.com>
2017-06-09 12:06:38 +0200

    add fontfamily scribble

--- packages/platon-components/src/design-system.js
+++ packages/platon-components/src/design-system.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Text, View} from 'react-primitives';
 import {Artboard, ColorTiles, COLOR_TILE_SIZE, COLOR_TILE_MARGIN} from '.';
+import {descriptionStyles, headlineStyles, sublineStyles} from './color-tile';
 
 const COLOR_TILES_IN_ROW = 3;
 const ARTBOARD_PADDING = 20;
@@ -21,5 +22,35 @@ export const DesignSystem = (props) => (
         <ColorTiles colors={props.colorTiles.colors}/>
       </Artboard>
     }
+      <Artboard
+        name={"Fontfamily"}
+        style={colorTilsArtboardStyle}
+        >
+        <View style={[{
+          height: COLOR_TILE_SIZE,
+          width: COLOR_TILE_SIZE,
+          margin: COLOR_TILE_MARGIN,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between'
+        }, {backgroundColor: '#f2f2f2'}]}>
+          <View style={[
+            descriptionStyles,
+            {backgroundColor: '#f2f2f2'}
+          ]}>
+            <Text style={{fontSize:30, lineHeight:40}}>
+              "Schönheit bietet eine natürliche Überlegenheit."
+            </Text>
+          </View>
+          <View style={[descriptionStyles, {}]}>
+            <Text style={headlineStyles}>
+              Helvetica Neue
+            </Text>
+            <Text style={sublineStyles}>
+              Regular
+            </Text>
+          </View>
+        </View>
+      </Artboard>
   </View>
 );
