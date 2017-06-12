d5a8f01a1db5900b9fbb8370e89bef8cd531541b
Maximilian <rittermax@gmail.com>
2017-06-12 18:47:19 +0200

    WIP Distance Tile

--- packages/platon-components/src/design-system.js
+++ packages/platon-components/src/design-system.js
@@ -1,9 +1,15 @@
 import React from 'react';
+<<<<<<< HEAD
 import {View} from 'react-primitives';
 import {Artboard} from './artboard';
 import {ColorTiles} from './color-tiles';
 import {Distances} from './distances';
 import {FontTiles} from './font-tiles';
+=======
+import {View, Text} from 'react-primitives';
+import {Artboard, FontTiles, ColorTiles, COLOR_TILE_SIZE, COLOR_TILE_MARGIN} from '.';
+import {descriptionStyles, headlineStyles, sublineStyles} from './color-tile';
+>>>>>>> WIP Distance Tile
 
 const TILE_SIZE = 300;
 const TILE_MARGIN = 10;
@@ -55,5 +61,99 @@ export const DesignSystem = props => (
             />
         </Artboard>
     }
+      <Artboard
+        name={'Vector'}
+        style={artboardStyle}
+        >
+        <View
+          style={{
+            backgroundColor: '#f2f2f2',
+            display: 'flex',
+            flexDirection: 'column',
+            height: COLOR_TILE_SIZE,
+            justifyContent: 'space-between',
+            margin: COLOR_TILE_MARGIN,
+            width: COLOR_TILE_SIZE
+          }}
+          >
+          <View
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              paddingTop: 20,
+              paddingBottom: 20
+            }}
+            >
+            <View
+              style={{
+                backgroundColor: '#fff',
+                borderStyle: 'solid',
+                borderColor: '#000000',
+                borderWidth: 1,
+                width: 150,
+                height: 50,
+                shadowColor: 'rgba(0,0,0,0.2)',
+                shadowOffset: {
+                  width: 0,
+                  height: 2
+                },
+                shadowRadius: 5
+              }}
+              />
+            <View
+              style={{
+                width: 150,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+              }}
+              >
+              <View
+                style={{
+                  backgroundColor: '#999',
+                  width: 150,
+                  height: 50, // Insert value of distance
+                  borderWidth: 0,
+                  borderBottomWidth: 1,
+                  borderBottomColor: '#000',
+                  borderStyle: 'solid'
+                }}
+                />
+              <View
+                style={{
+                  backgroundColor: '#222',
+                  width: 50
+                }}
+                >
+                <Text
+                  style={{
+                    fontSize: 12,
+                    lineHeight: 16,
+                    color: '#fff',
+                    textAlign: 'center',
+                    paddingTop: 5,
+                    paddingBottom: 5
+                  }}
+                  >
+                  ⤓ M {/* Insert distance name */}
+                </Text>
+              </View>
+            </View>
+          </View>
+          <View
+            style={descriptionStyles}
+            >
+            <Text style={headlineStyles}>
+              {'outside Margin M'}
+            </Text>
+            <Text style={sublineStyles}>
+              {'outside margin M'}
+            </Text>
+          </View>
+        </View>
+      </Artboard>
   </View>
 );
