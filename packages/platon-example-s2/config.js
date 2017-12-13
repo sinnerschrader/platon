c69dc061f3589be5547e58a56193a6b78e786782
RitterMax <maximilian.ritter@sinnerschrader.com>
2017-12-13 14:31:42 +0100

    Designsystem S2 adding more config data

--- packages/platon-example-s2/config.js
+++ packages/platon-example-s2/config.js
@@ -1,40 +1,42 @@
 export default {
   colors: {
-    name: 'S2 Colors',
+    name: 'colors',
     tokens: [
       {
         name: 'S2 BLUE',
-        description: 'Primary color',
         value: '#4502DA'
-      },
+      }
+    ]
+  },
+  fonts: {
+    name: 'fonts',
+    tokens: [
       {
-        name: 'S2 RED',
-        value: '#FF0354'
-      },
+        name: 'Maison Bold',
+        value: 'Maison-Bold'
+      }
+    ]
+  },
+  texts: {
+    name: 'texts',
+    tokens: [
       {
-        name: 'S2 GREEN',
-        value: '#03FFB7'
-      },
-      {
-        name: 'S2 BLACK',
-        value: '#1E212A'
-      },
-      {
-        name: 'S2 GREY',
-        value: '#E8ECEF'
-      },
-      {
-        name: 'S2 WHITE',
-        value: '#ffffff'
+        name: 'Order 1',
+        value: {
+          fontFamily: 'Maison-Bold',
+          fontSize: 40,
+          lineHeight: 52,
+          textAlign: 'left',
+          letterSpacing: 1
+        }
       }
     ]
   },
   sizes: {
-    name: 'S2 Sizes',
+    name: 'S2 Distances',
     tokens: [
       {
-        name: '320',
-        description: 'Size variables in Viewports <= 320',
+        name: 'mobile',
         value: [
           {
             name: 'XXS',
@@ -72,290 +74,156 @@ export default {
       }
     ]
   },
-  fonts: {
-    name: 'S2 Fonts',
-    tokens: [
-      {
-        name: 'Maison',
-        description: 'Regular',
-        value: 'Maison-Regular'
-      },
-      {
-        name: 'Maison',
-        description: 'Bold',
-        value: 'Maison-Bold'
-      },
-      {
-        name: 'Maison',
-        description: 'Light',
-        value: 'Maison-Light'
-      },
-      {
-        name: 'Maison',
-        description: 'Italic',
-        value: 'Maison-Italic'
-      },
-      {
-        name: 'Maison Mono',
-        value: 'MaisonMono-Regular'
-      },
-      {
-        name: 'Maison Mono',
-        description: 'Bold',
-        value: 'MaisonMono-Bold'
-      },
-      {
-        name: 'Maison Mono',
-        description: 'Light',
-        value: 'MaisonMono-Light'
-      },
-      {
-        name: 'Maison Mono',
-        description: 'Italic',
-        value: 'MaisonMono-Italic'
-      },
-      {
-        name: 'Noe',
-        value: 'NoeText-Regular'
-      }
-    ]
-  },
-  spaces: {
-    name: 'S2 Spaces',
-    tokens: [
-      {
-        name: 'All Margin M',
-        description: 'Primary outer spacing',
-        value: {
-          direction: 'outside',
-          top: {
-            name: 'M',
-            value: 18
-          },
-          right: {
-            name: 'M',
-            value: 18
-          },
-          bottom: {
-            name: 'M',
-            value: 18
-          },
-          left: {
-            name: 'M',
-            value: 18
-          }
-        }
-      },
-      {
-        name: 'Dented',
-        value: {
-          direction: 'inside',
-          top: 18,
-          right: 18,
-          bottom: 18,
-          left: {
-            name: 'XL',
-            value: 36
-          }
-        }
-      },
-      {
-        name: 'Horizontal Padding M',
-        value: {
-          direction: 'inside',
-          right: {
-            name: 'M',
-            value: 18
-          },
-          left: {
-            name: 'M',
-            value: 18
-          }
-        }
-      },
-      {
-        name: 'Horizontal Margin M',
-        value: {
-          direction: 'outside',
-          right: {
-            name: 'M',
-            value: 18
-          },
-          left: {
-            name: 'M',
-            value: 18
-          }
-        }
-      }
-    ]
-  },
-  texts: {
-    name: 'S2 Text Styles',
-    tokens: [
-      {
-        name: 'Headline 1',
-        description: 'Text style for primary headlines on any page',
-        value: {
-          fontFamily: 'Maison-Bold',
-          fontSize: 90,
-          lineHeight: 90,
-          textAlign: 'center'
-        }
-      },
-      {
-        name: 'Headline 2',
-        value: {
-          fontFamily: 'Maison-Bold',
-          fontSize: 48,
-          lineHeight: 68,
-          letterSpacing: 1
-        }
-      },
-      {
-        name: 'Headline 2 serif',
-        value: {
-          fontFamily: 'NoeText-Regular',
-          fontSize: 32,
-          lineHeight: 40,
-          textAlign: 'left'
-        }
-      }
-    ]
-  },
   vectors: {
     name: 'S2 Vectors',
     tokens: [
       {
-        name: 'Top M',
-        description: 'May have a description',
-        value: {
-          length: {
-            name: 'M',
-            value: 50
-          },
-          origin: 'top',
-          direction: 'outside'
-        }
-      },
-      {
-        name: 'Right M',
-        value: {
-          length: 50,
-          origin: 'right',
-          direction: 'outside'
-        }
-      },
-      {
-        name: 'Bottom M',
-        value: {
-          length: 50,
-          origin: 'bottom',
-          direction: 'outside'
-        }
-      },
-      {
-        name: 'Left M',
-        value: {
-          length: 50,
-          origin: 'left',
-          direction: 'outside'
-        }
-      },
-      {
-        name: 'Right L',
-        value: {
-          length: 300,
-          origin: 'right',
-          direction: 'outside'
-        }
-      },
-      {
-        name: 'Left L',
-        value: {
-          length: 300,
-          origin: 'left',
-          direction: 'outside'
-        }
-      },
-      {
-        name: 'Top L',
-        value: {
-          length: 300,
-          origin: 'top',
-          direction: 'outside'
-        }
-      },
-      {
-        name: 'Bottom L',
-        value: {
-          length: 300,
-          origin: 'bottom',
-          direction: 'outside'
-        }
-      },
-      {
-        name: 'Top M',
+        name: 'M',
         value: {
           length: 50,
           origin: 'top',
-          direction: 'inside'
+          direction: 'outside'
         }
       },
       {
-        name: 'Right M',
-        value: {
-          length: 50,
-          origin: 'right',
-          direction: 'inside'
-        }
+        distanceName: 'M',
+        distanceValue: 50,
+        vectorName: 'Top M',
+        vectorValue: 'Top M',
+        vectorOrigin: 'top',
+        vectorDirection: 'outside'
       },
       {
-        name: 'Bottom M',
-        value: {
-          lengthName: 'M',
-          length: 50,
-          origin: 'bottom',
-          direction: 'inside'
-        }
+        distanceName: 'M',
+        distanceValue: 50,
+        vectorName: 'Right M',
+        vectorValue: 'Right M',
+        vectorOrigin: 'right',
+        vectorDirection: 'outside'
       },
       {
-        name: 'Left M',
-        value: {
-          length: 50,
-          origin: 'left',
-          direction: 'inside'
-        }
+        distanceName: 'M',
+        distanceValue: 50,
+        vectorName: 'Bottom M',
+        vectorValue: 'Bottom M',
+        vectorOrigin: 'bottom',
+        vectorDirection: 'outside'
       },
       {
-        name: 'Top L',
-        value: {
-          length: 300,
-          origin: 'top',
-          direction: 'inside'
-        }
+        distanceName: 'M',
+        distanceValue: 50,
+        vectorName: 'Left M',
+        vectorValue: 'Left M',
+        vectorOrigin: 'left',
+        vectorDirection: 'outside'
       },
       {
-        name: 'Bottom L',
-        value: {
-          length: 300,
-          origin: 'bottom',
-          direction: 'inside'
-        }
+        distanceName: 'L',
+        distanceValue: 300,
+        vectorName: 'Right L',
+        vectorValue: 'Right L',
+        vectorOrigin: 'right',
+        vectorDirection: 'outside'
       },
       {
-        name: 'Right L',
-        value: {
-          length: 300,
-          origin: 'right',
-          direction: 'inside'
-        }
+        distanceName: 'L',
+        distanceValue: 300,
+        vectorName: 'Left L',
+        vectorValue: 'Left L',
+        vectorOrigin: 'left',
+        vectorDirection: 'outside'
       },
       {
-        name: 'Left L',
+        distanceName: 'L',
+        distanceValue: 300,
+        vectorName: 'Top L',
+        vectorValue: 'Top L',
+        vectorOrigin: 'top',
+        vectorDirection: 'outside'
+      },
+      {
+        distanceName: 'L',
+        distanceValue: 300,
+        vectorName: 'Bottom L',
+        vectorValue: 'Bottom L',
+        vectorOrigin: 'bottom',
+        vectorDirection: 'outside'
+      },
+      {
+        distanceName: 'M',
+        distanceValue: 50,
+        vectorName: 'Top M',
+        vectorValue: 'Top M',
+        vectorOrigin: 'top',
+        vectorDirection: 'inside'
+      },
+      {
+        distanceName: 'M',
+        distanceValue: 50,
+        vectorName: 'Right M',
+        vectorValue: 'Right M',
+        vectorOrigin: 'right',
+        vectorDirection: 'inside'
+      },
+      {
+        distanceName: 'M',
+        distanceValue: 50,
+        vectorName: 'Bottom M',
+        vectorValue: 'Bottom M',
+        vectorOrigin: 'bottom',
+        vectorDirection: 'inside'
+      },
+      {
+        distanceName: 'M',
+        distanceValue: 50,
+        vectorName: 'Left M',
+        vectorValue: 'Left M',
+        vectorOrigin: 'left',
+        vectorDirection: 'inside'
+      },
+      {
+        distanceName: 'L',
+        distanceValue: 300,
+        vectorName: 'Top L',
+        vectorValue: 'Top L',
+        vectorOrigin: 'top',
+        vectorDirection: 'inside'
+      },
+      {
+        distanceName: 'L',
+        distanceValue: 300,
+        vectorName: 'Bottom L',
+        vectorValue: 'Bottom L',
+        vectorOrigin: 'bottom',
+        vectorDirection: 'inside'
+      },
+      {
+        distanceName: 'L',
+        distanceValue: 300,
+        vectorName: 'Right L',
+        vectorValue: 'Right L',
+        vectorOrigin: 'right',
+        vectorDirection: 'inside'
+      },
+      {
+        distanceName: 'L',
+        distanceValue: 300,
+        vectorName: 'Left L',
+        vectorValue: 'Left L',
+        vectorOrigin: 'left',
+        vectorDirection: 'inside'
+      }
+    ]
+  },
+  spaces: {
+    name: 'Spaces',
+    tokens: [
+      {
+        name: 'Test',
         value: {
-          length: 300,
-          origin: 'left',
-          direction: 'inside'
+          size: 2,
+          margin: 50,
+          direction: 'Left'
         }
       }
     ]
