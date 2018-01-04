ae9730c773281051659d9759bf240b1ecd3491d3
Mario Nebl <hello@mario-nebl.de>
2018-01-04 15:44:08 +0100

    docs: describe <Text/>

--- packages/platon-components/src/text/demo.js
+++ packages/platon-components/src/text/demo.js
@@ -4,7 +4,36 @@ import {Text} from '.';
 import {DemoContainer} from '../demo-container';
 
 const DOC = `
+# Text
 
+\`<Text/>\` gathers all styling relevant to a body of text,
+including \`fontFamily\`, \`fontSize\`, \`lineHeight\`, \`color\`, \`textTransform\`, \`textAlign\` and \`letterSpacing\`.
+
+This allow to define context-aware sizes,
+under one name e.g. when adapting to different
+screen sizes.
+
+## Usage
+
+\`\`\`js
+function SpaceDemo() {
+  return (
+    <View>
+      <Text
+        name="A"
+        description="Text styles are made up of many properties"
+        fontFamily={{name: 'Georgia', value: 'Georgia'}}
+        fontSize={50}
+        lineHeight={{name: 'A', value: 60}}
+        color={{name: 'Black', value: '#000'}}
+        >
+          Die Philosophie bietet mir einen Hafen,
+          während ich andere mit den Stürmen kämpfen sehe.
+      </Text>
+    </View>
+  );
+}
+\`\`\`
 `;
 
 const StyledRow = styled.View`
